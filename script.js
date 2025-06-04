1cde360789de2063440a127040ab6eabb384d099
Avraneel Pal <avraneelpal22@gmail.com>
2025-06-04 22:52:55 +0530

    Add grid style and preliminary support for mouse hover

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 
 let body = document.querySelector("body");
 let container = document.createElement("div");
+container.classList.toggle("container");
 body.appendChild(container);
 
-let divArray = [];
-
 for(let i = 1; i <= 16; i++) {
-    let myDiv = document.createElement("div");
-    container.appendChild(myDiv);
+    let rowContainer = document.createElement("div");
+    rowContainer.classList.toggle("row-container");
+    for(let j = 1; j <= 16; j++) {
+        let myDiv = document.createElement("div");
+        myDiv.classList.toggle("row-item");
+        myDiv.addEventListener("mouseover", (myDiv) => {
+            myDiv.style.backgroundColor = "red";
+        });
+        rowContainer.appendChild(myDiv);
+    }
+    container.appendChild(rowContainer);
 }
-
