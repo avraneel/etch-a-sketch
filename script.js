243cdbc06a846677a097a8b902b713f94bd0f4ee
Avraneel Pal <avraneelpal22@gmail.com>
2025-06-05 22:00:50 +0530

    Add initial support for reset button

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 "use strict";
 
 let body = document.querySelector("body");
+
+const button = document.createElement("button");
 let container = document.createElement("div");
+
+button.textContent = "Reset Me";
+button.classList.toggle("reset-button");
+button.addEventListener("click", getInput);
+
 container.classList.toggle("container");
+
+function drawGrid(x) {
+    for(let i = 1; i <= x; i++) {
+        let rowContainer = document.createElement("div");
+        rowContainer.classList.toggle("row-container");
+        for(let j = 1; j <= x; j++) {
+            let myDiv = document.createElement("div");
+            myDiv.classList.toggle("row-item");
+            myDiv.addEventListener("mouseover", function () {
+                this.style.backgroundColor = "red";
+            });
+            rowContainer.appendChild(myDiv);
+        }
+        container.appendChild(rowContainer);
+    }
+}
+
+function getInput() {
+    let x = "";
+    do {
+        x = prompt("Enter number of squares per size. Must be < 100.");
+    } while(x > 100);
+    drawGrid(x);
+}
+
+body.appendChild(button);
 body.appendChild(container);
 
-for(let i = 1; i <= 16; i++) {
-    let rowContainer = document.createElement("div");
-    rowContainer.classList.toggle("row-container");
-    for(let j = 1; j <= 16; j++) {
-        let myDiv = document.createElement("div");
-        myDiv.classList.toggle("row-item");
-        myDiv.addEventListener("mouseover", (myDiv) => {
-            myDiv.style.backgroundColor = "red";
-        });
-        rowContainer.appendChild(myDiv);
-    }
-    container.appendChild(rowContainer);
-}
+drawGrid(16);
